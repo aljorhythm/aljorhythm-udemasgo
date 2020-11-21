b972635ff95e902374f77c095a194a2bbd06c166
Joel Lim <joel.lim@circles.asia>
2020-11-21 20:52:38 +0800

    hello world

---
+++ helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aljorhythm/udemasgo-sec1-intro/utility"
+)
+
+func main() {
+	fmt.Println(utility.GetHello())
+}
---
+++ utility/utility.go
@@ -0,0 +1,5 @@
+package utility
+
+func GetHello() string {
+	return "Hello world!"
+}
